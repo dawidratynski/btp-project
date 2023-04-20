7e9bc074858d71869ec97a5b883cd3ab5c63b2e2
Dawid <dawidratynski@gmail.com>
2023-04-20 20:37:01 +0200

    Update quote stub id generation

--- src/quote/test/stubs/quote.stub.ts
+++ src/quote/test/stubs/quote.stub.ts
@@ -2,7 +2,7 @@ import { Quote } from "src/quote/quote.entity";
 
 export const quoteStub = (): Quote => {
     return {
-        id: 1,
+        id: Date.now(),
         ticker: "testTicker",
         timestamp: new Date("3000-01-08T02:05:06.000Z"),
         price: 32.41
