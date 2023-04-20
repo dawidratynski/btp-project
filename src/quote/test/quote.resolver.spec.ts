7a432b4df002a8ed146d1b5f56436ab4aaaaf3bc
Dawid <dawidratynski@gmail.com>
2023-04-20 21:02:34 +0200

    Add unit tests for quote resolver

--- src/quote/test/quote.resolver.spec.ts
+++ src/quote/test/quote.resolver.spec.ts
@@ -1,10 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { resolve } from 'path/posix';
 import { DbModule } from '../../db/db.module';
+import { Quote } from '../quote.entity';
 import { QuoteResolver } from '../quote.resolver';
 import { QuoteService } from '../quote.service';
+import { quoteStub } from './stubs/quote.stub';
+
+jest.mock('./../quote.service.ts');
 
 describe('QuoteResolver', () => {
   let resolver: QuoteResolver;
+  let service: QuoteService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -13,9 +19,51 @@ describe('QuoteResolver', () => {
     }).compile();
 
     resolver = module.get<QuoteResolver>(QuoteResolver);
+    service = module.get<QuoteService>(QuoteService);
+    jest.clearAllMocks();
   });
 
-  it('should be defined', () => {
+  test('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('getQuotes', () => {
+    describe('when getQuotes is called', () => {
+      let quotes: Quote[];
+      let expectedQuotes: Quote[];
+
+      beforeEach(async () =>{
+        quotes = await resolver.getQuotes()
+        expectedQuotes = await service.getQuotes()
+      })
+
+      test('then it should call QuoteService::getQuotes', () => {
+        expect(service.getQuotes).toBeCalled();
+      })
+
+      test('then should return the same as service.getQuotes', () => {
+        expect(quotes).toEqual(expectedQuotes);
+      })
+    })
+  });
+
+  describe('addQuote', () => {
+    describe('when addQuote is called', () => {
+      let returnQuote: Quote;
+      let expectedReturnQuote: Quote;
+
+      beforeEach(async () =>{
+        returnQuote = await resolver.addQuote(quoteStub());
+        expectedReturnQuote = await service.addQuote(quoteStub());
+      })
+
+      test('then it should call QuoteService::addQuote', () => {
+        expect(service.addQuote).toBeCalled();
+      })
+
+      test('then should return the same as service.addQuote', () => {
+        expect(returnQuote).toEqual(expectedReturnQuote);
+      })
+    })
   });
 });
