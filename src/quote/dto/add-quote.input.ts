a72d39f0449113035f18be44f28ef45331755765
Dawid <dawidratynski@gmail.com>
2023-04-20 16:55:34 +0200

    Add API input validation via ValidationPipe

--- src/quote/dto/add-quote.input.ts
+++ src/quote/dto/add-quote.input.ts
@@ -1,14 +1,19 @@
 import { Field, InputType, Float, GraphQLTimestamp } from "@nestjs/graphql";
+import { IsAlphanumeric, IsNotEmpty } from "class-validator";
 import { GraphQLDateTime} from 'graphql-iso-date';
 
 @InputType()
 export class AddQuoteInput{
     @Field(type => String)
+    @IsAlphanumeric()
+    @IsNotEmpty()
     ticker:string;
 
     @Field(type => GraphQLDateTime)
+    @IsNotEmpty()
     timestamp:Date;
 
     @Field(type => Float)
+    @IsNotEmpty()
     price:number;
 }
