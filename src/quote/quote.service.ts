804769f873b742863e9491d7ee1b001d2c8929c7
Dawid <dawidratynski@gmail.com>
2023-04-20 18:59:03 +0200

    Add database error handling

--- src/quote/quote.service.ts
+++ src/quote/quote.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { PG_CONNECTION } from 'src/constants';
 import { AddQuoteInput } from './dto/add-quote.input';
 import { Quote } from './quote.entity';
@@ -8,18 +8,30 @@ export class QuoteService {
 
     constructor(@Inject(PG_CONNECTION) private conn: any) {}
 
+
     async getQuotes() {
-        const res = await this.conn.query( `SELECT * FROM quote`);
-        return res.rows as Quote[];
+        const query_str = `SELECT * FROM quote`;
+        try {
+            const res = await this.conn.query(query_str);
+            return res.rows as Quote[];
+        } catch {
+            throw new HttpException("Failed to connect to database", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
+
 
     async addQuote(addQuoteInput : AddQuoteInput):Promise<Quote>{
         const query_str = "INSERT INTO quote(ticker, timestamp, price) VALUES ( '" + 
                         addQuoteInput.ticker.toString() + "', '" + addQuoteInput.timestamp.toISOString() + "','" + 
                         addQuoteInput.price.toString() + "' ) RETURNING * ;";
 
-        const newQuote = await this.conn.query(query_str);
+        try{
+            const newQuote = await this.conn.query(query_str);
+            return (newQuote.rows as Quote[])[0]
+        } catch {
+            throw new HttpException("Failed to connect to database", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
-        return (newQuote.rows as Quote[])[0];
+        //return (newQuote.rows as Quote[])[0];
     }
 }
