592498aef7006e9ddc07025b24a95cdb0a26d6e8
Dawid <dawidratynski@gmail.com>
2023-04-20 21:01:33 +0200

    Change quote stub id back to a constant

--- src/quote/test/stubs/quote.stub.ts
+++ src/quote/test/stubs/quote.stub.ts
@@ -2,7 +2,7 @@ import { Quote } from "src/quote/quote.entity";
 
 export const quoteStub = (): Quote => {
     return {
-        id: Date.now(),
+        id: 1682017199084,
         ticker: "testTicker",
         timestamp: new Date("3000-01-08T02:05:06.000Z"),
         price: 32.41
