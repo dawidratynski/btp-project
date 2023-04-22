31159bea940b3da6bf158a08a7cf1d71cdd63c75
Dawid <dawidratynski@gmail.com>
2023-04-22 12:39:51 +0200

    Add concurrency tests to quote resolver

--- src/quote/__mocks__/quote.service.ts
+++ src/quote/__mocks__/quote.service.ts
@@ -1,15 +1,15 @@
 import { quoteStub } from "../test/stubs/quote.stub";
 
 export const QuoteService = jest.fn().mockReturnValue({
-    getQuotes: jest.fn().mockResolvedValue(
-        [
+    getQuotes: jest.fn().mockImplementation(() => {
+        return [
             quoteStub(),
             quoteStub(),
             quoteStub(),
-        ]
-    ),
+        ];
+    }),
 
-    addQuote: jest.fn().mockResolvedValue(
-        quoteStub()
-    )
+    addQuote: jest.fn().mockImplementation(() => {
+        return quoteStub();
+    })
 })
--- src/quote/test/quote.resolver.spec.ts
+++ src/quote/test/quote.resolver.spec.ts
@@ -43,7 +43,37 @@ describe('QuoteResolver', () => {
       test('then should return the same as service.getQuotes', () => {
         expect(quotes).toEqual(expectedQuotes);
       })
-    })
+    });
+
+    describe('when getQuotes is called multiple times concurrently', () => {
+      let elapsed_time: number;
+      const sleep_time = 100;
+      function sleep(time_in_ms: number) {
+        return new Promise(resolve => setTimeout(resolve, time_in_ms));
+      }
+      const {performance} = require('perf_hooks');
+
+      beforeEach(async () =>{
+        service.getQuotes = (async () => {
+          await sleep(sleep_time);
+          return [quoteStub(), quoteStub(), quoteStub()];
+        })
+
+        let start_time = performance.now();
+        const query1 = resolver.getQuotes();
+        const query2 = resolver.getQuotes();
+        const query3 = resolver.getQuotes();
+        await query1;
+        await query2;
+        await query3;
+        let end_time = performance.now();
+        elapsed_time = end_time - start_time;
+      })
+
+      test('then it should run queries concurrently', () => {
+        expect(elapsed_time).toBeLessThan(3 * sleep_time);
+      })
+    });
   });
 
   describe('addQuote', () => {
@@ -63,6 +93,37 @@ describe('QuoteResolver', () => {
       test('then should return the same as service.addQuote', () => {
         expect(returnQuote).toEqual(expectedReturnQuote);
       })
-    })
+    });
+    describe('when addQuote is called multiple times concurrently', () => {
+      let elapsed_time: number;
+      const sleep_time = 100;
+      function sleep(time_in_ms: number) {
+        return new Promise(resolve => setTimeout(resolve, time_in_ms));
+      }
+      const {performance} = require('perf_hooks');
+
+      beforeEach(async () =>{
+        service.addQuote = (async () => {
+          await sleep(sleep_time);
+          return quoteStub();
+        })
+
+        let start_time = performance.now();
+        const query1 = resolver.addQuote(quoteStub());
+        const query2 = resolver.addQuote(quoteStub());
+        const query3 = resolver.addQuote(quoteStub());
+        await query1;
+        await query2;
+        await query3;
+        let end_time = performance.now();
+        elapsed_time = end_time - start_time;
+      })
+
+      test('then it should run queries concurrently', () => {
+        expect(elapsed_time).toBeLessThan(3 * sleep_time);
+      })
+    });
   });
+
+  
 });
