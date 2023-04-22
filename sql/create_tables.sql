d0af1f4a1eea30fbeaafb1820fcd0f9b9b9f1cd0
Dawid <dawidratynski@gmail.com>
2023-04-22 20:09:38 +0200

    Rename table from quote to quotes

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS quote (
+CREATE TABLE IF NOT EXISTS quotes (
     id SERIAL PRIMARY KEY,
     ticker varchar(50) NOT NULL,
     timestamp TIMESTAMP NOT NULL,
--- src/quote/quote.service.ts
+++ src/quote/quote.service.ts
@@ -9,7 +9,7 @@ export class QuoteService {
     constructor(@Inject(PG_CONNECTION) public conn: any) {}
 
     async getQuotes():Promise<Quote[]> {
-        const query_str = `SELECT * FROM quote`;
+        const query_str = `SELECT * FROM quotes`;
         try {
             const res = await this.conn.query(query_str);
             return res.rows as Quote[];
@@ -19,7 +19,7 @@ export class QuoteService {
     }
 
     async addQuote(addQuoteInput : AddQuoteInput):Promise<Quote>{
-        const query_str = "INSERT INTO quote(ticker, timestamp, price) VALUES ( '" + 
+        const query_str = "INSERT INTO quotes(ticker, timestamp, price) VALUES ( '" + 
                         addQuoteInput.ticker.toString() + "', '" + 
                         addQuoteInput.timestamp.toISOString() + "','" + 
                         addQuoteInput.price.toString() + "' ) RETURNING * ;";
