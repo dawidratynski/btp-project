541f0aec6bc04b839a5c9ab052f8cec48ec7e8ce
Dawid <dawidratynski@gmail.com>
2023-05-14 16:30:20 +0200

    Change method of building queries

--- src/quote/quote.service.ts
+++ src/quote/quote.service.ts
@@ -23,13 +23,13 @@ export class QuoteService {
 
 
     async addQuote(addQuoteInput : AddQuoteInput):Promise<Quote>{
-        const query_str = "INSERT INTO quotes(ticker, timestamp, price) VALUES ( '" + 
-                        addQuoteInput.ticker.toString() + "', '" + 
-                        addQuoteInput.timestamp.toISOString() + "','" + 
-                        addQuoteInput.price.toString() + "' ) RETURNING * ;";
+        const query_str = "INSERT INTO quotes(ticker, timestamp, price) VALUES ( $1, $2, $3 ) RETURNING *;"
+        const query_values = [addQuoteInput.ticker.toString(), addQuoteInput.timestamp.toISOString(), addQuoteInput.price.toString()];
+
+        // TO DO: change query to work with new database
 
         try{
-            const newQuote = await this.conn.query(query_str);
+            const newQuote = await this.conn.query(query_str, query_values);
             return (newQuote.rows as Quote[])[0];
         } catch (err) {
             throw new HttpException(err.message, HttpStatus.INTERNAL_SERVER_ERROR);
