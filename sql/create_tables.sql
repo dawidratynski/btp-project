ffd6eb10bcbd6fccfac119a6cd0ec378466bac23
Dawid <dawidratynski@gmail.com>
2023-05-14 16:26:20 +0200

    Add tickers table to database creation script

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,6 +1,11 @@
+CREATE TABLE IF NOT EXISTS tickers (
+    id SERIAL PRIMARY KEY,
+    ticker varchar(50) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS quotes (
     id SERIAL PRIMARY KEY,
-    ticker varchar(50) NOT NULL,
+    ticker_id INTEGER REFERENCES tickers (id),
     timestamp TIMESTAMP NOT NULL,
     price DOUBLE PRECISION NOT NULL
 );
